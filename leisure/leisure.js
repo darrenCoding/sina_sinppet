fb8f59b7a5c90d193d59537f000cc4d66c1390ce
cookiy <cookiy8511@gmail.com>
2014-12-30 22:45:23 +0800

    change

--- leisure/leisure.js
+++ leisure/leisure.js
@@ -1,3 +1,17 @@
-;(function () {
-	// body...
+;(function() {
+    Array.prototype.indexOf = function(val) {
+        for (var i = 0; i < this.length; i++) {
+            if (this[i] == val) return i;
+        }
+        return -1;
+    };
+    Array.prototype.remove = function (val) {
+    	var index = this.indexof(val);
+    	if (isNan(index) || index >= this.length) {
+            return false
+        };
+    	if (index > -1) {
+    		this.splice(index,1);
+    	};
+    };
 })();
