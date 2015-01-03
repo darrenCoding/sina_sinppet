c7c243c75fbbe2b6ae3b130f13e113ed0ad095f9
cookiy <cookiy8511@gmail.com>
2015-01-03 21:10:42 +0800

    change

--- leisure/leisure.js
+++ leisure/leisure.js
@@ -18,3 +18,34 @@
     	};
     };
 })();
+
+
+// 单例模式
+var factory = function () {
+    return function () {
+        // 
+    }
+};
+var jQuery = new factory();
+
+// jQuery自执行模式
+(function (window,undefined) {
+    var jQuery = function () {
+        // body...
+    };
+    window.jQuery = window.$ = jQuery;
+})(winodw);
+
+
+if (typeof module === "object" && typeof module.exports === "object") {
+    module.exports = global.document ?
+        factory(global, true) :
+        function(w) {
+            if (!w.document) {
+                throw new Error("jQuery requires a window with a document");
+            }
+            return factory(w);
+    };
+} else {
+    factory(global);
+}
