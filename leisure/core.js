01c793376f490d33c83984a02248788e8a9ba926
cookiy <cookiy8511@gmail.com>
2015-01-08 22:01:23 +0800

    change

--- leisure/core.js
+++ leisure/core.js
@@ -6,27 +6,6 @@ var factory = function () {
 };
 var jQuery = new factory();
 
-// jQuery自执行模式
-(function (window,undefined) {
-    var jQuery = function () {
-        // body...
-    };
-    window.jQuery = window.$ = jQuery;
-})(winodw);
-
-if (typeof module === "object" && typeof module.exports === "object") {
-    module.exports = global.document ?
-        factory(global, true) :
-        function(w) {
-            if (!w.document) {
-                throw new Error("jQuery requires a window with a document");
-            }
-            return factory(w);
-    };
-} else {
-    factory(global);
-};
-
 var $$ = ajQuery = function(selector){
     if (!(this.instanceof ajQuery)){
         return new ajQuery(selector);        
