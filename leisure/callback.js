809cae2fe5787e06bd709b8b4c19a392bed3fe3f
cookiy8511@gmail.com <cookiy8511@gmail.com>
2015-01-09 10:38:26 +0800

    change

--- leisure/callback.js
+++ leisure/callback.js
@@ -1,7 +1,4 @@
 // jQuery 回调 $.callback
-
-
-
 Deferred: function(func) {
     var tuples = [
         ["resolve", "done", jQuery.Callbacks("once memory"), "resolved"],
@@ -14,4 +11,45 @@ Deferred: function(func) {
 ajax: function(url,options) {
 	deferred = jQuery.Deferred();
 	completeDeferred = jQuery.Callbacks("once memory");
+};
+
+function Callbacks (options) {
+	var list = [];
+	var self;
+	var firingStart;
+	var memory;
+	function _fire (data) {
+		memory = options === 'memory' && data;
+		firingIndex = firingStart || 0;
+		firingStart = 0;
+		firingLength = list.length;
+		for (; list && firingIndex < firingLength; firingIndex++) {
+			if (list[firingIndex](data) == false && options === 'stopOnFalse') {
+				break;
+			};
+		}
+	};
+	self = {
+		add:function (fn) {
+			var start = list.length;
+			if (options == 'unique') {
+				if (-1 == list.indexOf(fn)) {
+					list.push(fn);
+				};
+			} else {
+				list.push(fn);
+			}
+			
+			if (memory) {
+				firingStart = start;
+				_fire(memory);
+			};
+		},
+		fire:function(args) {
+			if (list) {
+				_fire(args);
+			};
+		}
+	};
+	return self;
 };
