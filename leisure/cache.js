a16adb22f4eea11c495c8ecb107b7421f7057263
cookiy <cookiy8511@gmail.com>
2015-01-13 10:11:02 +0800

    

--- leisure/cache.js
+++ leisure/cache.js
@@ -1,8 +1,21 @@
 function Data() {
-    Object.defineProperty(this.cache = {}, {
-    	get:function () {
-    		return{};
-    	}
+	Object.defineProperty(this.cache = {}, {
+		get: function() {
+			return {};
+		},
 
-    });
-}
+	});
+	this.expando = jQuery.expando + _Math.random();
+};
+
+Data.uid = 1;
+Data.accepts = jQuery.acceptData;
+Data.prototype = {
+	key: function() {},
+	set: function() {},
+	get: function() {},
+	remove: function() {},
+	hasData: function() {},
+	access: function() {}
+};
+
