7827b06e8708c0449105ba4c27f37b83bc19e9ff
cookiy <cookiy8511@gmail.com>
2015-01-14 09:00:29 +0800

    change

--- leisure/asyn.js
+++ leisure/asyn.js
@@ -1,28 +1,35 @@
-jQuery.extend ({
-	Deferred:function (func) {
-		var tuples:[
-			['resolve','done',jQuery.callbacks('once memory'),'resolved'],
-			['reject','fail',jQuery.callbacks('once memory'),'rejected'],
-			['notify','progress',jQuery.callbacks('memory')],
-		],
-		state = 'pending',
-		promise = {
-			state:function () {},
-			always:function () {},
-			then:function () {},
-			promise:function (obj) {}
-		},
-		deferred = {},
-		promise.pipe = promise.then;
-		jQuery.each(tuples,function (i,tuple) {
-			deferred[tuples[0]] = function () {
-				// body...
-			}
-		});
+jQuery.extend({
+        Deferred: function(func) {
+            var tuples: [
+                ['resolve', 'done', jQuery.callbacks('once memory'), 'resolved'],
+                ['reject', 'fail', jQuery.callbacks('once memory'), 'rejected'],
+                ['notify', 'progress', jQuery.callbacks('memory')],
+            ],
+                state = 'pending',
+                promise = {
+                    state: function() {},
+                    always: function() {},
+                    then: function() {},
+                    promise: function(obj) {}
+                },
+                deferred = {},
+                promise.pipe = promise.then;
+            jQuery.each(tuples, function(i, tuple) {
+                    deferred[tuples[0]] = function() {
+                        deferred[tuple[0] + "With"](this === deferred ? promise : this, arguments);
+                        return this;
+                    };
+                    deferred[tuple[0] + "With"] = list.fireWith;
+                }
+            });
 
-	},
-	when:function (func) {
-		return deferred.promise();
-	}
+            promise.promise(deferred);
+            if (func) {
+            	func.call(deferred,deferred);
+            };
+
+    },
+    when: function(func) {
+        return deferred.promise();
+    }
 });
-
