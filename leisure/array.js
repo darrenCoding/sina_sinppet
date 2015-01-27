c3797e961eb9dc3279e62489789888f339c93593
cookiy <cookiy8511@gmail.com>
2015-01-27 21:37:48 +0800

    change

---
+++ leisure/array.js
@@ -0,0 +1,40 @@
+;(function() {
+    Array.prototype.isArray = function () {
+        // body...
+    };
+    Array.prototype.observe = function () {
+        // body...
+    };
+    Array.prototype.of = function () {
+        // body...
+    };
+    Array.prototype.every = function () {
+        // body...
+    };
+    Array.prototype.isArray = function () {
+        // body...
+    };
+    Array.prototype.isArray = function () {
+        // body...
+    };
+	Array.prototype.contains = function(arr,val) {
+		// body...
+	};
+
+    Array.prototype.indexOf = function(val) {
+        for (var i = 0; i < this.length; i++) {
+            if (this[i] == val) return i;
+        }
+        return -1;
+    };
+    
+    Array.prototype.remove = function (val) {
+    	var index = this.indexof(val);
+    	if (isNan(index) || index >= this.length) {
+            return false
+        };
+    	if (index > -1) {
+    		this.splice(index,1);
+    	};
+    };
+})();
